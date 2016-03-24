8f5ef42d50d01f8c98538a190687858fcb3be924
David Winter <i@djw.me>
2016-03-24 09:56:32 +0000

    Initial Commit

---
+++ Noted/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Noted
+//
+//  Created by David Winter on 24/03/2016.
+//  Copyright © 2016 Made Tech. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Noted/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Noted
+//
+//  Created by David Winter on 24/03/2016.
+//  Copyright © 2016 Made Tech. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
